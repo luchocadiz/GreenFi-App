685d84ce3e6a5cd54e4daeda694f5ab3cda9e611
davideq5 <davideq555@gmail.com>
2025-08-31 05:12:29 -0300

    <fix> Correccion de catch error

--- packages/hardhat/deploy/deploy_proposal_registry.ts
+++ packages/hardhat/deploy/deploy_proposal_registry.ts
@@ -29,6 +29,8 @@ const deployProposalRegistry: DeployFunction = async function (hre: HardhatRunti
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
+  }).catch(error => {
+    console.error(error);
   });
 };
 
